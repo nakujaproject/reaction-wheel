8f17511af29aa6b9f157da5489603a523c24c621
TheParadox20 <onyambusamson@gmail.com>
2021-12-30 23:37:51 +0300

    variable update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,4 @@
 #include <Arduino.h>
- 
-/*
-    Kalman Filter for MPU6050. Output for processing with Serial Plotter.
-    Web: https://indobot.co.id
-    (created) 2021 by Zamisyak Oby Indobot Academy
-    The Fastest Way to Electronics Mastery
-*/
-
 #include <Wire.h>
 #include <MPU6050.h>
 #include <KalmanFilter.h>
@@ -16,10 +8,7 @@ MPU6050 mpu;
 KalmanFilter kalmanX(0.001, 0.003, 0.03);
 KalmanFilter kalmanY(0.001, 0.003, 0.03);
 
-float accPitch = 0;
 float accRoll = 0;
-
-float kalPitch = 0;
 float kalRoll = 0;
 
 void setup() 
@@ -35,8 +24,8 @@ void setup()
   // Calibration of gyroscopes. When calibrating, the sensor must be stationary.
   mpu.calibrateGyro();// If you don't want to calibrate, comment on this line.
 
-  // Create a label on each output.
-  Serial.println("accPitch,kalPitch,accRoll,kalRoll");
+  // Create a label on each output. 
+  Serial.println("| accRoll \t| kalRoll\t|");
 }
 
 void loop()
@@ -45,27 +34,18 @@ void loop()
   Vector gyr = mpu.readNormalizeGyro();
 
   // Calculating Pitch &amp; Roll of accelerometer (degrees)
-  accPitch = -(atan2(acc.XAxis, sqrt(acc.YAxis*acc.YAxis + acc.ZAxis*acc.ZAxis))*180.0)/M_PI;
-  accRoll  = (atan2(acc.YAxis, acc.ZAxis)*180.0)/M_PI;
+  accRoll  = (atan2(acc.YAxis, acc.ZAxis)*180.0)/M_PI; // acctual roll
 
   // Kalman filter
-  kalPitch = kalmanY.update(accPitch, gyr.YAxis);
-  kalRoll = kalmanX.update(accRoll, gyr.XAxis);
-
-  // View pitch data before in Filter
-  Serial.print(accPitch);
-  Serial.print(",");
-
-  // View Pitch data after Filter
-  Serial.print(kalPitch);
-  Serial.print(",");
+  kalRoll = kalmanX.update(accRoll, gyr.XAxis); //required variable
 
   // View Roll data before in Filter
+  Serial.print("| ");
   Serial.print(accRoll);
-  Serial.print(",");
+  Serial.print("\t| ");
 
   // View Roll data after in Filter
   Serial.print(kalRoll);
-  Serial.println();
+  Serial.println("\t|");
 
 }
